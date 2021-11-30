c7531ab7f84dcb7dc9db32f502f30ec6a380ea6a
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2021-11-30 18:33:32 -0300

    generator log

--- generator.py
+++ generator.py
@@ -12,16 +12,23 @@ IQ = 10
 selected_scenarios=[]
 
 gen_log =  GL.generator_log(True) 
-ID_dat="Model_Name"
+ID_dat="Test_example"
 
 datasheetmodel = model_gen.create_single_datasheet(SQ,RC,MP,IQ,selected_scenarios,ID_dat)
 
 
 print("----------------------------------------------------------------")
-# print(json.dumps(datasheetmodel))
 
 
 gen_log.print_inc()
 
 print("----------------------------------------------------------------")
 print(json.dumps(datasheetmodel))
+
+test_dir ='.//testCases//'
+name_json =test_dir+ID_dat+'.json'
+with open(name_json, "w") as outfile:
+    json.dump(datasheetmodel, outfile)
+    
+    
+    
